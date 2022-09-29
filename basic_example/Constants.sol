abda48dcbf41954fa8655bbab8fc100358f6c52b
Remix Workspace <>
2022-09-29 09:13:00 +0800

    add examples of variables, constant, ether, immutable,

---
+++ basic_example/Constants.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+contract Constants {
+
+    // 常量的编码约定为大写字母
+    address public constant MY_ADDRESS = 0x777788889999AaAAbBbbCcccddDdeeeEfFFfCcCc;
+    uint public constant MY_UINT = 123;
+
+}
