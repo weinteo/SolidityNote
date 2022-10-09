449487ee360bef91dea295952e04b400414caea3
Remix Workspace <>
2022-10-09 09:03:49 +0800

    add if/else, loop and  mapping example

---
+++ basic_example/Mapping.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+contract Mapping {
+    
+    mapping(address => uint) public myMap;
+    
+    function get(address _addr) public view returns (uint){
+        return myMap[_addr];
+    }
+
+    function set(address _addr, uint _i) public {
+        myMap[_addr] = _i;
+    }
+
+    function remove(address _addr) public{
+        delete myMap[_addr];
+    }
+    
+}
