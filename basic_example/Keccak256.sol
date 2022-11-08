37c30eca09febb1920f9361711aaa26d2b22547d
Remix Workspace <>
2022-11-08 09:03:37 +0800

    modify keccak256 example

--- basic_example/Keccak256.sol
+++ basic_example/Keccak256.sol
@@ -10,11 +10,13 @@ contract HashFunction {
         return keccak256(abi.encodePacked(_text, _num, _addr));
     }
 
+    // hash碰撞
     function collision(string memory _text, string memory _anotherText)
         public
         pure
         returns (bytes32)
     {
+        // 使用encodePacked会出现碰撞，不同的值得到相同的结果，比如：
         // encodePacked(AA, ABBB) -> AAABBB
         // encodePacked(AAA, BBB) -> AAABBB
         return keccak256(abi.encodePacked(_text, _anotherText));
@@ -22,11 +24,10 @@ contract HashFunction {
 }
 
 contract GuessTheMagicWord {
-
     bytes32 public answer =
         0x60298f78cc0b47170ba79c10aa3851d7648bd96f2f8e46a19dbc777c36fb0c00;
 
     function guess(string memory _word) public view returns (bool) {
-        return keccak256(abi.encodePacked(_word)) == answer;  // Magic word is "Solidity"
+        return keccak256(abi.encodePacked(_word)) == answer; // Magic word is "Solidity"
     }
 }
